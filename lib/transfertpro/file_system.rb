16f33f643b4d394a5aafd7be42085bbb1f6e81ca
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2024-03-08 11:40:26 -1000

    fix upload of files : use binary mode

--- lib/transfertpro/file_system.rb
+++ lib/transfertpro/file_system.rb
@@ -230,7 +230,7 @@ module Transfertpro
       chunk_count = chunk_count(input_file_path)
       offset = 0
       loop do
-        Tempfile.create(["tp", extension]) do |chunk_file|
+        Tempfile.create(["tp", extension], binmode: true) do |chunk_file|
           chunk = create_chunk(chunk_file, file)
           params = chunk_params(chunk, chunk_count, file_description, offset, share_id)
           upload_chunk(params, chunk_file)
